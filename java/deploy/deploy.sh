9eeea2ffeac9bb69b6acd01b0313f4051062e3b4
Josh Long <josh@joshlong.com>
2020-10-16 13:50:11 -0700

    deploy

--- java/deploy/deploy.sh
+++ java/deploy/deploy.sh
@@ -6,11 +6,12 @@ PROJECT_ID=${GKE_PROJECT:-pgtm-jlong}
 cd $(dirname $0)/..
 root_dir=$(pwd)
 
-#mvn -DskipTests=true clean spring-boot:build-image
-#image_id=$(docker images -q $APP_NAME)
-#docker tag "${image_id}" gcr.io/${PROJECT_ID}/${APP_NAME}
-#docker push gcr.io/${PROJECT_ID}/${APP_NAME}
-#docker pull gcr.io/${PROJECT_ID}/${APP_NAME}:latest
+mvn -DskipTests=true clean spring-boot:build-image
+image_id=$(docker images -q $APP_NAME)
+
+docker tag "${image_id}" gcr.io/${PROJECT_ID}/${APP_NAME}
+docker push gcr.io/${PROJECT_ID}/${APP_NAME}
+docker pull gcr.io/${PROJECT_ID}/${APP_NAME}:latest
 
 kubectl create deployment ${APP_NAME} --image=gcr.io/${PROJECT_ID}/${APP_NAME}
 kubectl expose deployment ${APP_NAME} --port=80 --target-port=8080 --name=${APP_NAME} --type=LoadBalancer
