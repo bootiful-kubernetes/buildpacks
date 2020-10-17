c54f44438ebbfdd6298abf20caf8def895aa0b11
Josh Long <josh@joshlong.com>
2020-10-17 02:09:12 -0700

    deploy

--- python/main.py
+++ python/main.py
@@ -1,18 +1,7 @@
 from sanic import Sanic
 from sanic.response import json
 
-'''
-This is a trivial asynchronous IO-centric Python webapplication. 
-
-Run build.sh to use Packeto to build an image 
-
-You can then run it using: 
-
-docker run -p 8080:8080  python-asyncio-web 
-'''
-
 app = Sanic('hello-python')
-
 
 @app.route("/")
 async def test(request):
